335cec7ef7cfe2356878b0a373c9689c1c66affb
Ekin KARAKOÇ <ekinkarakoc1@gmail.com>
2025-01-30 18:20:05 +0300

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/d-logo.png";
+import user from "../assets/images/user-avatar.svg";
 import {
   HiHome,
   HiMagnifyingGlass,
@@ -11,30 +12,74 @@ import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "./HeaderItem";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
   const menu = [
     { name: "HOME", icon: HiHome },
     { name: "SEARCH", icon: HiMagnifyingGlass },
     { name: "WATCH LIST", icon: HiPlus },
     { name: "ORIGINALS", icon: HiStar },
-    { name: "SERIES", icon: HiTv },
     { name: "MOVIES", icon: HiPlayCircle },
+    { name: "SERIES", icon: HiTv },
   ];
+  console.log(toggle);
 
   return (
-    <div>
-      <img
-        src={logo}
-        alt="disney-logo"
-        className="w-[80px] md:w-[115px] object-cover"
-      />
-      {menu.map((item, index) => (
-        <HeaderItem
-          key={index}
-          name={item.name}
-          icon={item.icon}
-          index={index}
+    <div className="flex items-center justify-between px-8 py-4">
+      <div className="flex items-center gap-8">
+        <img
+          src={logo}
+          alt="disney-logo"
+          className="w-[80px] md:w-[115px] object-cover"
         />
-      ))}
+        <div className="hidden md:flex gap-8">
+          {menu.map((item, index) => (
+            <HeaderItem
+              key={index}
+              name={item.name}
+              Icon={item.icon}
+              index={index}
+            />
+          ))}
+        </div>
+        <div className="flex gap-8 md:hidden">
+          {menu.map(
+            (item, index) =>
+              index < 3 && (
+                <HeaderItem
+                  key={index}
+                  name={""}
+                  Icon={item.icon}
+                  index={index}
+                />
+              )
+          )}
+          <div className="md:hidden">
+            <div
+              onClick={() => {
+                setToggle(!toggle);
+              }}
+            >
+              <HeaderItem name="" Icon={HiDotsVertical} />
+            </div>
+            {toggle && (
+              <div className="absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-4 px-5">
+                {menu
+                  .filter((_, index) => index > 2)
+                  .map((item, index) => (
+                    <HeaderItem
+                      key={index}
+                      name={item.name}
+                      Icon={item.icon}
+                      index={index}
+                    />
+                  ))}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <img src={user} alt="user-avatar" className="w-[40px] rounded-full" />
     </div>
   );
 };
--- src/components/HeaderItem.jsx
+++ src/components/HeaderItem.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const HeaderItem = ({ index }) => {
-  return <div>headeritem: {index}</div>;
+const HeaderItem = ({ name, Icon }) => {
+  return (
+    <div className="text-white flex items-center  gap-3 text-[15px] font-semibold cursor-pointer relative group mb-2">
+      <Icon />
+      <h2 className="md:block  relative group">
+        {name}
+        <span className="absolute bottom-[-4px] left-0 w-0 h-[2px] bg-white transition-all duration-300 group-hover:w-full"></span>
+      </h2>
+
+      <span className="absolute hidden  bottom-[-6px] left-0 w-0 h-[2px] bg-white transition-all duration-300 group-hover:w-full md:block"></span>
+    </div>
+  );
 };
 
 export default HeaderItem;
